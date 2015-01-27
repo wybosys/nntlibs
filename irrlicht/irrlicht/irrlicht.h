31dae3a406c904781222557917b19c7b3ea808f4
wybosys <wybosys@gmail.com>
2015-01-27 19:02:18 +0800

    libs

---
+++ irrlicht/irrlicht/irrlicht.h
@@ -0,0 +1,13 @@
+//
+//  irrlicht.h
+//  irrlicht
+//
+//  Created by wybo on 15/1/27.
+//  Copyright (c) 2015年 wybosys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface irrlicht : NSObject
+
+@end
---
+++ irrlicht/irrlicht/irrlicht.m
@@ -0,0 +1,13 @@
+//
+//  irrlicht.m
+//  irrlicht
+//
+//  Created by wybo on 15/1/27.
+//  Copyright (c) 2015年 wybosys. All rights reserved.
+//
+
+#import "irrlicht.h"
+
+@implementation irrlicht
+
+@end
